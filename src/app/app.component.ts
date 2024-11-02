6da613e6410ca69c8305901eedff64a9da2d64d8
Mbungai-Francesco <mbungaifrancesco2@gmail.com>
2024-11-02 16:16:22 +0100

    info page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/shared/navbar/navbar.component";
+import { ButtonPrevComponent } from "./components/shared/button-prev/button-prev.component";
+import { ButtonNextComponent } from "./components/shared/button-next/button-next.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, ButtonPrevComponent, ButtonNextComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/shared/button-next/button-next.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-next',
+  standalone: true,
+  imports: [],
+  templateUrl: './button-next.component.html',
+  styleUrl: './button-next.component.css'
+})
+export class ButtonNextComponent {
+
+}
